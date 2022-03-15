b8a882833eae05aab778254aeb2b65d1f0d667b5
Johannes Link <business@johanneslink.net>
2022-03-15 14:40:14 +0100

    Expanded calculator example

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
-def sum(a, b):
-    return a + b
+from functools import reduce
+
+def sum(*addends):
+    # return reduce(lambda a, b: a + b, addends, 0)
+    sum = 0
+    for addend in addends:
+        if (addend < 0):
+            raise ValueError("Addends must not be negative")
+        sum += addend
+    return sum
 
 if __name__ == '__main__':
     print('3 + 5 =', sum(3, 5))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,16 @@
+import pytest
+
 from calculator import sum
 
-def test_sum():
+def test_sum_of_none():
+    assert sum() == 0
+
+def test_sum_of_two():
     assert sum(3, 2) == 5
 
-def test_sum_three_values():
-    assert sum(3, 2, 1) == 6
+def test_sum_of_four():
+    assert sum(3, 2, 1, 10) == 16
+
+def test_cannot_sum_negative_numbers():
+    with pytest.raises(ValueError):
+        sum(3, -1)
