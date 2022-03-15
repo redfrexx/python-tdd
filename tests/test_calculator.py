a01a660e690c2c42906d2db899482ef174caf54c
Johannes Link <business@johanneslink.net>
2022-03-15 14:49:01 +0100

    Add problem description with initial inbox

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,10 @@
+# Develop a function sum() to add up an unknown number of non-negative numbers:
+# - no numbers
+# - single number
+# - several numbers
+# - decimals
+# - negative number -> Error
+
 import pytest
 
 from calculator import sum
@@ -10,6 +17,9 @@ def test_sum_of_two():
 
 def test_sum_of_four():
     assert sum(3, 2, 1, 10) == 16
+
+def test_sum_of_decimals():
+    assert sum(3.1, 4.2, 100.01) == 107.31
 
 def test_cannot_sum_negative_numbers():
     with pytest.raises(ValueError):
