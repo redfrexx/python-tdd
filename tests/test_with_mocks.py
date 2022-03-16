691f887de5a05c78f66e530c4199776e491f1c48
Johannes Link <business@johanneslink.net>
2022-03-16 10:36:23 +0100

    Rounded up work on mocking tests

--- tests/test_with_mocks.py
+++ tests/test_with_mocks.py
@@ -1,5 +1,5 @@
 import pytest
-# import pytest_mock
+from pytest_mock import MockerFixture
 from unittest.mock import Mock
 
 
@@ -26,8 +26,30 @@ def test_mocked_method(my_object):
     assert my_object.greet() == "Hello, Frank"
 
 
+def test_mocked_method_with_side_effect(my_object):
+    def side_effect(*args, **kwargs):
+        return "Catherine"
+
+    mocked_name = Mock(side_effect=side_effect)
+    my_object.name = mocked_name
+    assert my_object.greet() == "Hello, Catherine"
+    mocked_name.assert_called()
+
+
 def test_mocked_class():
     mock = Mock(MyClass)
-    mock.greet.return_value = "Hello, Frank"
-    assert mock.greet() == "Hello, Frank"
+    mock.greet.return_value = "Hello, Esra"
+    assert mock.greet() == "Hello, Esra"
     assert isinstance(mock, MyClass)
+    assert mock.greet.call_count == 1
+    mock.greet.assert_called()
+
+
+# Using mocker fixture (provided by pytest_mock) allows to skip unittest.mock imports
+def test_mocked_class_with_pytest_mocker(mocker: MockerFixture):
+    mock = mocker.Mock(MyClass)
+    mock.greet.return_value = "Hello, Esra"
+    assert mock.greet() == "Hello, Esra"
+    assert isinstance(mock, MyClass)
+    assert mock.greet.call_count == 1
+    mock.greet.assert_called()
