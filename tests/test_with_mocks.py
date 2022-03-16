98e5714cb74ad6e5dc8c55a45bfa4476f53f81aa
Johannes Link <business@johanneslink.net>
2022-03-16 09:39:50 +0100

    More mocking tests

--- tests/test_with_mocks.py
+++ tests/test_with_mocks.py
@@ -1,6 +1,6 @@
 import pytest
 # import pytest_mock
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 
 class MyClass():
@@ -15,18 +15,19 @@ class MyClass():
 def my_object():
     return MyClass()
 
-def test_mocked_class():
-    mock = Mock(MyClass)
-    mock.greet.return_value = "Hello, Frank"
-    assert mock.greet() == "Hello, Frank"
 
 def test_greet(my_object):
     assert my_object.greet() == "Hello, Johannes"
 
-def test_patched_object(my_object):
-    with patch.object(my_object, 'greet') as mock_greet:
-        def side_effect(a):
-            return "Hello, Frank"
-        mock_greet.side_effect = side_effect
-    # The following does not work. I wonder why.
-    # assert my_object.greet() == "Hello, Frank"
+
+def test_mocked_method(my_object):
+    mocked_name = Mock(return_value="Frank")
+    my_object.name = mocked_name
+    assert my_object.greet() == "Hello, Frank"
+
+
+def test_mocked_class():
+    mock = Mock(MyClass)
+    mock.greet.return_value = "Hello, Frank"
+    assert mock.greet() == "Hello, Frank"
+    assert isinstance(mock, MyClass)
