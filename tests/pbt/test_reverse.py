99872ba0da5a0567c1068a77a50f0971f07c720d
Johannes Link <business@johanneslink.net>
2022-03-17 15:26:28 +0100

    Extracted non-empty-lists strategy

--- tests/pbt/test_reverse.py
+++ tests/pbt/test_reverse.py
@@ -8,13 +8,18 @@ def reverse(list):
     return clone
 
 
+# nonEmptyLists = lists(integers()).filter(lambda l: len(l) > 0)
+# nonEmptyLists = lists(integers()).filter(lambda l: bool(l))
+nonEmptyLists = lists(integers(), min_size=1)
+
+
 @given(lists(integers()))
 def test_double_reverse_creates_original(original: list[int]):
     # print(original)
     assert reverse(reverse(original)) == original
 
 
-@given(lists(integers()).filter(lambda l: len(l) > 0))
+@given(nonEmptyLists)
 def test_reverse_makes_last_first(original: list[int]):
     # print(original)
     reversed_ = reverse(original)
